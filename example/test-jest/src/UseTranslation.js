6a2b5102087528b7b9d80a5a11ea6c8b3a5a4e6e
Gilad Taase <giladtaase@screenovate.com>
2021-06-17 00:54:27 +0300

    Removed example

--- example/test-jest/src/UseTranslation.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { useTranslation } from 'react-i18next';
-
-export default function CustomComponent() {
-  const { t } = useTranslation();
-
-  return <div>{t('description.part2')}</div>;
-}
--- example/test-jest/src/WithTranslation.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { withTranslation } from 'react-i18next';
-
-export function CustomComponent({ t }) {
-  return <div>{t('description.part3')}</div>;
-}
-
-export default withTranslation()(CustomComponent);
